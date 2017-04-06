b55bc00bfb3225c995c3629fc62a9b0fed346d40
toale <toale@Austins-HP-Omen>
2017-04-06 16:40:11 -0400

    Added JUnit test

---
+++ com/hangman/test/EnglishDictionaryTest.java
@@ -0,0 +1,19 @@
+package com.hangman.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.hangman.data.*;
+
+public class EnglishDictionaryTest {
+
+
+	@Test
+	public void isNotNull_test() {
+		EnglishDictionary ed = new EnglishDictionary();
+		Assert.assertNotEquals(null, ed);
+	}
+
+}
