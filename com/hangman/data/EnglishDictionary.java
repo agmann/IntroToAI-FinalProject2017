0d9153419595cb0e0b5c596bd59ef00b645308c2
toale <toale@Austins-HP-Omen>
2017-04-06 16:32:05 -0400

    Added comments.

--- com/hangman/data/EnglishDictionary.java
+++ com/hangman/data/EnglishDictionary.java
@@ -1,5 +1,12 @@
 package com.hangman.data;
 
+/*
+ * The EnglishDictionary class serves as an abstraction from the MySQL database that contains
+ * words from the English dictionary. 
+ * The methods provided in this class will be used in the com.hangman.game package to 
+ * parse available English words so that the AI player can guess the word. This class should not contain 
+ * any game-play/AI implementation, solely code that serves data from a MySQL server. 
+ */
 public class EnglishDictionary {
 
 
