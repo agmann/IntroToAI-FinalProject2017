30e143acb88137180b2fdbc284afbb005f56882d
toale <toale@Austins-HP-Omen>
2017-04-11 16:33:30 -0400

    Refined Database class, outsourced server configuration keys to a file.

--- com/hangman/game/HangmanGame.java
+++ com/hangman/game/HangmanGame.java
@@ -1,10 +1,13 @@
 package com.hangman.game;
 
+import com.hangman.data.Database;
+
 public class HangmanGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		Database db = new Database(Database.DEFUALT_SERVER_INFO);
+		System.out.println(db.getServerDetails());
 	}
 
 }
